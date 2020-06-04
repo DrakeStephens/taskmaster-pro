4def7bbf29880baa51199f0e9a0c8a73e989b33f
Drake Stephens <drake.stephens56@gmail.com>
2020-06-04 17:45:25 -0600

    added dates

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -1,11 +1,30 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+ 
+  var daTe = $(taskEl).find("span").text().trim();
+
+
+  var time = moment(daTe, "L").set("hour", 17);
+
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } 
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+  
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+  
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +32,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -85,39 +105,38 @@ $(".list-group").on("click", "span", function(){
     .text()
     .trim();
   
-  var dateInput = $("<input>")  
+  var dateInput = $("<input>")
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
 
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   dateInput.trigger("focus");
 
 });
 
-$(".list-group").on("blur","input[type='text']",function(){
-  var date = $(this)
-    .val()
-    .trim();
+$(".list-group").on("change", "input[type='text']", function() {
+  var date = $(this).val();
 
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+  var index = $(this).closest(".list-group-item").index();
 
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
-  
   tasks[status][index].date = date;
   saveTasks();
-  
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
-  
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -197,6 +216,7 @@ $(".card .list-group").sortable({
         .trim();
       
       tempArr.push({
+        
         text: text,
         date: date
       });
@@ -226,3 +246,6 @@ $("#trash").droppable({
   }
 });
 
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
